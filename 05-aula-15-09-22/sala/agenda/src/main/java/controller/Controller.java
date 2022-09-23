53fe5206e0ef65b6fd0a1270d5e7b24e6f339a0d
Auto Logon <autologon@CSED.LAB>
2022-09-22 21:52:47 -0300

    save commit

--- 05-aula-15-09-22/sala/agenda/src/main/java/controller/Controller.java
+++ 05-aula-15-09-22/sala/agenda/src/main/java/controller/Controller.java
@@ -5,28 +5,38 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.DAO;
+
 import java.io.IOException;
 
 /**
  * Servlet implementation class Controller
  */
+@WebServlet(urlPatterns = { "/controller", "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private DAO con;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public Controller() {
+		super();
+		con = new DAO();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		con.testeConexao();
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
 }
--- 05-aula-15-09-22/sala/agenda/src/main/java/model/DAO.java
+++ 05-aula-15-09-22/sala/agenda/src/main/java/model/DAO.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DAO {
 
+	String driver = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/dbagenda?useTimezone=true%serverTimezone=UTC";
+	String user = "root";
+	String password = "root";
+	
+	private Connection conectar() {
+		Connection con = null;
+		
+		try {
+			Class.forName(driver);
+			
+			con = DriverManager.getConnection(url, user, password);	
+		}catch (Exception e) {
+			System.out.println(e.getMessage());			
+		}
+		
+		return con;
+	}
+	
+	public void testeConexao() {
+		this.conectar();
+	}
 }
